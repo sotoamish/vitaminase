d11b83c939f8f125eb997bb64404b5b70ae027b8
AlmaSZ <49698648+sotoamish@users.noreply.github.com>
2020-06-09 19:58:07 -0500

    Add files via upload
    el js que imprime las listas?

--- Navbarvita.js
+++ Navbarvita.js
@@ -1,7 +1,10 @@
 //for all pages when page is loaded create navbar
 document.addEventListener("DOMContentLoaded", function() {
-    // function fron this script
+    // function from this script
     printNavBar();
+    printVitaminList();
+    printRetosList();
+    printEetdList();
 });
 
 //generate the NavBar
@@ -13,7 +16,8 @@ function printNavBar() {
             </button>
                 
             <div class="navbar-collapse collapse" id="navbarToggler" style="">
-                <div class="navbar-nav mr-auto mt-2 mt-lg-0">           
+                <div class="navbar-nav brand_container mr-auto mt-2 mt-lg-0">           
+                    <a href="index.html"><img id="navBrand" src="img/vitamin/alasyraiceslogo.png" alt="Vamos al inicio de la pÃ¡gina Alas y RaÃ­ces" /> </a>
                 </div>
                 <ul class="navbar-nav">
                     <li class="nav-item">
@@ -29,7 +33,7 @@ function printNavBar() {
                         <a class="nav-link" href="ebooks.html"><i class="fas fa-lg fa-book-open"></i> e-books </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="marzo.html"><i class="far fa-lg fa-calendar-alt"></i> Cartelera </a>
+                        <a class="nav-link" href="index.html"><i class="far fa-lg fa-calendar-alt"></i> Cartelera </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#suscribe_block"><i class="far fa-lg fa-envelope"></i> Suscríbete </a>
@@ -39,9 +43,17 @@ function printNavBar() {
         </nav>
 
         <div class="col-12 subNav">
-            <a href="index.html">
-                <img src="../img/LogoVitaAyR.png" alt="Bienvenidos a Vitamina Se de Alas y Raíces" class="navBrand">
-            </a>                
+            <div class="row">
+                <div class="col-lg-4 logo_container">
+                    <img id="logoVitaminas" src="img/vitamin/vslh.png" alt="logo de VitaminaSé" />     
+                </div>
+                <div class="col-lg-8 button_container">
+                    <button type="button" class="btn navBtns" onclick="changeActiveButton(this)" data-toggle="collapse" data-target="#queEs" aria-expanded="false"> ¿Qué es Vitamina Sé? </button>
+                    <button type="button" class="btn navBtns" onclick="changeActiveButton(this)" data-toggle="collapse" data-target="#comoJuego" aria-expanded="false"> ¿Cómo se juega? </button> 
+                    <a href="img/neo/recetario.pdf" target="_blank"><button type="button" class="btn"> Recetario descargable </button> </a>
+                    <a href="/registro.html" target="_blank"><button type="button" class="btn"> Regístrate en los RETOS  </button> </a>
+                </div>
+            </div>
         </div>
             `;
     const navAyr = document.getElementsByClassName("nav_vita")[0];
@@ -53,7 +65,175 @@ function printNavBar() {
             // console.log( elm.querySelector("a").href );
             elm.classList.add("active");
         }        
-    });
-    
+    });  
 
+} 
+
+//print vitamin data list 
+function printVitaminList() {
+    var accordionList = document.getElementById("accordionList");
+    
+    let toPrint = '';
+    //if vitamin data exists generate the list
+    if (vitamin_data !== undefined) {
+        for (let i = 0; i < Object.keys(vitamin_data).length; i++) {
+            let key = Object.keys(vitamin_data)[i];            
+            let show = (i === 0)? 'show' : '' ;
+
+            toPrint += `
+                <div class="card">
+                    <div class="card-header">
+                        <button class="btn btn-block" type="button" data-toggle="collapse" data-target="#c${key}" 
+                            onClick="changeVideo(${key}, 'capsula')"
+                            aria-expanded="true" aria-controls="c1">
+                            <img src="img/vitamin/capsulita0.png">
+                            <b>Cápsula ${key}</b>
+                        </button>
+                    </div>
+
+                    <div id="c${key}" class="collapse ${show}" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <b><i>${vitamin_data[key]['name']}</i></b>, <br> ${vitamin_data[key]['type']} con ${vitamin_data[key]['author']}. <br>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            if( i === 0) {
+                changeVideo( `${key}`, "capsula" ); 
+            }
+        }
+    }
+
+    accordionList.innerHTML = toPrint;
+}
+
+//print retos list 
+function printRetosList() {
+    var retosList = document.getElementById("retosList");
+
+    let toPrint = '';
+    // if retos data exists generate the list 
+    if( retos_data !== undefined){
+        for (let i = 0; i < Object.keys(retos_data).length; i++) {
+            let key = Object.keys(retos_data)[i];            
+            
+            toPrint += `
+                <div class="card">
+                    <div class="card-header">
+                        <button class="btn btn-block" type="button" data-toggle="collapse" data-target="#r${key}" 
+                            onClick="changeVideo(${key}, 'reto')"
+                            aria-expanded="true" aria-controls="c1">
+                            <img src="img/vitamin/capsulita1.png">
+                            <b>Te reto a... ${key}</b>
+                        </button>
+                    </div>
+
+                    <div id="r${key}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <b><i>${retos_data[key]['name']}</i></b>, <br> ${retos_data[key]['type']} con ${retos_data[key]['author']}. <br>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+        }
+    }
+
+    retosList.innerHTML = toPrint ;
+}
+
+//print eetd list 
+function printEetdList() {
+    var eetdList = document.getElementById("eetdList");
+
+    let toPrint = '';
+    // if eetd data exists generate the list 
+    if( eetd_data !== undefined){
+        for (let i = 0; i < Object.keys(eetd_data).length; i++) {
+            let key = Object.keys(eetd_data)[i];            
+            
+            toPrint += `
+                <div class="card">
+                    <div class="card-header">
+                        <button class="btn btn-block" type="button" data-toggle="collapse" data-target="#r${key}" 
+                            onClick="changeVideo(${key}, 'eetd')"
+                            aria-expanded="true" aria-controls="c1">
+                            <img src="img/vitamin/capsulita2.png">
+                            <b>Especial en tu día: ${key}</b>
+                        </button>
+                    </div>
+
+                    <div id="r${key}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <b><i>${eetd_data[key]['name']}</i></b>, <br> ${eetd_data[key]['type']} con ${eetd_data[key]['author']}. <br>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+        }
+    }
+
+    eedtList.innerHTML = toPrint ;
+}
+
+//change the video source 
+function changeVideo( key, origin ) {
+    
+    let videoTag = document.getElementById("vitamin_video_player");
+    let videoDescription = document.getElementById("videoDescription");
+    let currentUrl = videoTag.src;
+
+    let videoBlock = document.getElementsByClassName("vitamina_description");
+    let vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    if( origin === "capsula" ) {
+        var data_origin = vitamin_data;
+    } else if( origin === "reto") {
+        var data_origin = retos_data;
+    } if( origin === "eetd") {
+        var data_origin = eetd_data;
+    }
+    
+    if( data_origin[key]['url'] != currentUrl) {
+        //set new src on the player
+        videoTag.src = data_origin[key]['url'];
+        //change video description 
+        videoDescription.innerHTML = data_origin[key]['description'];
+
+        if (vw <= 992) {
+            videoBlock[0].scrollIntoView({ block: "start", behavior: "smooth" });
+        }
+    }
+    
+}
+
+
+//toggle navBtns and their infoPanels
+function changeActiveButton( elm ) {
+    navBtns = Array.prototype.slice.call(
+        document.getElementsByClassName("navBtns")
+    );
+    arrInfo = Array.prototype.slice.call(
+        document.getElementsByClassName("actInfo")
+    );
+
+    objective = elm.dataset.target;
+    objective = objective.replace('#','');
+
+    navBtns.forEach(btn => {
+        if (btn !== elm) {
+            if(btn.classList.contains("active")) {
+                btn.classList.remove("active");
+            }        
+        }
+    });
+    arrInfo.forEach(col => {
+        if( col.id !== objective) {
+            col.classList.remove("show");
+        } 
+    });
+
+    elm.classList.toggle("active");
 }
